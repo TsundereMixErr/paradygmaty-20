20103694bee4a1ff80e27f07b8c2f994e81743b9
TsundereMixErr <mlibura@interia.pl>
2020-11-18 16:05:10 +0100

    Zad 3 BFS

--- pp_lab/src/l4.scala
+++ pp_lab/src/l4.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object l4 {
   sealed trait BT[+A]
   case object Empty extends BT[Nothing]
@@ -36,12 +38,6 @@ object l4 {
       case _ => Empty
     }
 
-  def preOrder[A](tree: BT[A]): String =
-    tree match {
-      case Empty => ""
-      case Node(value, leftNode, rightNode) => value + " " + preOrder(leftNode) + " " + preOrder(rightNode)
-    }
-
   //ZADANIE 3 (4pkt)
   def removeDuplicatesDFS(tree1: BT[Int], tree2: BT[Int]): (BT[Int], BT[Int]) = {
     (tree1, tree2) match {
@@ -54,6 +50,82 @@ object l4 {
         else (Node(value1, leftSubtree._1, rightSubtree._1), Node(value2, leftSubtree._2, rightSubtree._2))
     }
   }
+
+  def removeDuplicatesBFS(tree1: BT[Int], tree2: BT[Int]): (BT[Int], BT[Int]) = {
+    def innerRemoveDuplicatesBFS(tree1: BT[Int], tree2: BT[Int], queue: List[(BT[Int], BT[Int])],numberOfNode: Int, even: Int):
+    (List[(Int, Int)], List[(Int, Int)]) =
+      queue match {
+        case Nil => (Nil, Nil)
+        case (Empty,Empty) :: _ => (Nil, Nil)
+        case (Node(value1, leftNode1, rightNode1), Node(value2, leftNode2, rightNode2)) :: tail =>
+          if(value1 == value2 && areSubtreesEqual(tree1, tree2)) {
+            if(tail == Nil) (Nil, Nil)
+            else innerRemoveDuplicatesBFS(queue.tail.head._1, queue.tail.head._2, queue.tail,
+              if(leftNode1 == Empty) numberOfNode
+              else numberOfNode + 1,
+              even + 1)
+          }
+          else if(value1 == value2) {
+            val queue2 = queue.tail ::: List((leftNode1, leftNode2), (rightNode1, rightNode2))
+            val result = innerRemoveDuplicatesBFS(queue2.tail.head._1, queue2.tail.head._2, queue2.tail,
+            if(leftNode1 == Empty) numberOfNode
+            else if(even % 2 == 0) 2 * numberOfNode
+            else 2 * numberOfNode + 1,
+              even + 1)
+            ((-1, numberOfNode) :: result._1, (-1, numberOfNode) :: result._2)
+          }
+          else {
+            val queue2 = queue ::: List((leftNode1, leftNode2), (rightNode1, rightNode2))
+            val result = innerRemoveDuplicatesBFS(queue2.tail.head._1, queue2.tail.head._2, queue2.tail, numberOfNode + 1, even + 1)
+            ((value1, numberOfNode) :: result._1, (value2, numberOfNode) :: result._2)
+          }
+      }
+    val list = innerRemoveDuplicatesBFS(tree1, tree2, List((tree1, tree2)), 1, 0)
+    (listToTree(1, countNodes(tree1), list._1), listToTree(1, countNodes(tree2), list._2))
+  }
+
+  //Funkcje pomocnicze:
+  def areSubtreesEqual[A](tree1: BT[A], tree2: BT[A]): Boolean = {
+    @tailrec
+    def areSubtreesEqualRec(xs: List[(BT[A], BT[A])]): Boolean =
+      xs match {
+        case Nil => true
+        case (Empty, Empty) :: _ => true
+        case (Node(value1, leftNode1, rightNode1), Node(value2, leftNode2, rightNode2)) :: tail =>
+          value1 == value2 && areSubtreesEqualRec(tail ::: List((leftNode1, leftNode2), (rightNode1, rightNode2)))
+      }
+    areSubtreesEqualRec(List((tree1, tree2)))
+  }
+
+  @tailrec
+  def checkIfContains[A](xs: List[(A, A)], element: A, index: Int): Int =
+    xs match {
+      case Nil => -1
+      case head :: tail => if(head._2 == element) index
+                           else checkIfContains(tail, element, index + 1)
+    }
+
+  def getValueAt[A](xs: List[A], index: Int): A = {
+    @tailrec
+    def getValueAtRec(xs: List[A], index: Int, current: Int): A =
+      current match {
+        case index => xs.head
+        case _ => getValueAtRec(xs.tail, index, current + 1)
+      }
+    getValueAtRec(xs, index, 0)
+  }
+
+  def listToTree(actualNode: Int, numberOfNodes: Int, listOfNodes: List[(Int,Int)]): BT[Int] = {
+    if (actualNode > numberOfNodes || checkIfContains(listOfNodes, actualNode, 0) == -1) Empty
+    else Node(getValueAt(listOfNodes, checkIfContains(listOfNodes, actualNode, 0))._1,
+      listToTree(actualNode*2, numberOfNodes, listOfNodes), listToTree(actualNode*2+1, numberOfNodes, listOfNodes))
+  }
+
+  def countNodes(tree: BT[Int]): Int =
+    tree match {
+      case Empty => 0
+      case Node(_, l, r) => 1 + countNodes(l) + countNodes(r)
+    }
 
 
   //ZADANIE 4 (5 pkt)
